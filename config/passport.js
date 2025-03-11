d66c3f753dac397881683269df52c38881ec1f7f
Shreejith Kumar Das S <shreejithisdumb@gmail.com>
2025-03-11 19:03:28 +0530

    Update passport.js to use async/await for deserializeUser

--- config/passport.js
+++ config/passport.js
@@ -23,10 +23,13 @@ passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
-passport.deserializeUser((id, done) => {
-  User.findById(id, (err, user) => {
-    done(err, user);
-  });
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id).exec();
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
 });
 
 module.exports = passport;
